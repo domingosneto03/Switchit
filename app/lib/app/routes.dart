d0839311061458a8d054de4bf6b4b71b7e1da399
Artur Telo <arturtelo88@hotmail.com>
2023-03-13 16:04:53 +0000

    Create routes.dart

---
+++ app/lib/app/routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:learningdart/ui/credits/credits_screen.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  CreditsScreen.routeName: (context) => const CreditsScreen(),
+};
