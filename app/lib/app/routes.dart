55443ad59a3614d2ca1c4d5105956dfe145261c7
Artur Telo <arturtelo88@hotmail.com>
2023-03-13 23:01:31 +0000

    Update routes

--- app/lib/app/routes.dart
+++ app/lib/app/routes.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:learningdart/ui/credits/credits_screen.dart';
+import '/ui/credits/credits_screen.dart';
+import '/ui/home/home_screen.dart';
+import '/ui/login/login_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
+  LoginScreen.routeName: (context) => const LoginScreen(),
+  HomeScreen.routeName: (context) => const HomeScreen(),
   CreditsScreen.routeName: (context) => const CreditsScreen(),
 };
--- app/lib/ui/home/home_screen.dart
+++ app/lib/ui/home/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:learningdart/ui/home/components/body.dart';
+import '/ui/home/components/body.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
+  static String routeName = "/home_screen";
 
   @override
   Widget build(BuildContext context) {
