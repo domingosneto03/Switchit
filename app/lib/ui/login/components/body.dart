8084e75dbcbcb49d041523be31462fde6652e4e6
Artur Telo <arturtelo88@hotmail.com>
2023-03-14 21:47:41 +0000

    Update login screen

--- app/lib/ui/login/components/body.dart
+++ app/lib/ui/login/components/body.dart
@@ -1,33 +1,132 @@
 import 'package:flutter/material.dart';
+import 'package:switchit/ui/login/components/square_tile.dart';
 import '/ui/home/home_screen.dart';
+import 'button.dart';
+import 'my_textfield.dart';
 
 class Body extends StatelessWidget {
-  const Body({super.key});
+  Body({super.key});
+
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  signUserIn(BuildContext context) {
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => const HomeScreen()),
+      (Route<dynamic> route) => false,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: SizedBox(
-        width: double.infinity,
-        child: Column(children: [
-          Expanded(
-              child: Align(
-            alignment: Alignment.bottomCenter,
-            child: TextButton(
-                style: const ButtonStyle(
-                  foregroundColor: MaterialStatePropertyAll(Colors.white),
-                  backgroundColor: MaterialStatePropertyAll(Colors.red),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Login Account',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              'Welcome back you\'ve been missed!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 25),
+            MyTextField(
+              controller: usernameController,
+              hintText: 'Email',
+              obscureText: false,
+            ),
+            const SizedBox(height: 10),
+            MyTextField(
+              controller: passwordController,
+              hintText: 'Password',
+              obscureText: true,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: const [
+                  Text(
+                    'Forgot Password?',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 25),
+            MyButton(
+              onTap: () {
+                signUserIn(context);
+              },
+            ),
+            const SizedBox(height: 50),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                children: [
+                  const Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text(
+                      'Or continue with',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                // google button
+                SquareTile(imagePath: 'assets/images/google.png'),
+                SizedBox(width: 25),
+              ],
+            ),
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  'Not a member?',
+                  style: TextStyle(color: Colors.white),
                 ),
-                onPressed: () {
-                  Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(builder: (context) => const HomeScreen()),
-                    (Route<dynamic> route) => false,
-                  );
-                },
-                child: const Text('Home Screen')),
-          ))
-        ]),
+                SizedBox(width: 4),
+                Text(
+                  'Register now',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
