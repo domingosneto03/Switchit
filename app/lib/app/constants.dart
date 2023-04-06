38bee26ffcd33cd4abafc745e2ba7f8f359786e1
Artur Telo <93836583+ArturTelo@users.noreply.github.com>
2023-04-06 14:48:04 +0100

    Create constants.dart

---
+++ app/lib/app/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final RegExp emailValidatorRegExp = RegExp(
+    r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+
+const MaterialColor colorPrimary = Colors.deepPurple;
