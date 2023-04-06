e7d6cefaf7552f005cac5a61f3aa1c1425fd6777
Artur Telo <93836583+ArturTelo@users.noreply.github.com>
2023-04-06 14:48:14 +0100

    Create user_realm.dart

---
+++ app/lib/database/user_realm.dart
@@ -0,0 +1,13 @@
+import 'package:realm/realm.dart';
+
+part 'user_realm.g.dart';
+
+@RealmModel()
+class _UserRealm {
+  int id = 1;
+  late String name;
+  late String surname;
+  late String email;
+  late String docId;
+  bool isRememberedDetails = false;
+}
