f67fb6ba6f7d15a09415f15aac22e5b0a7907276
Artur Telo <93836583+ArturTelo@users.noreply.github.com>
2023-04-06 14:48:17 +0100

    Create network_auth_controller.dart

---
+++ app/lib/network/network_auth_controller.dart
@@ -0,0 +1,129 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:switchit/database/database_realm.dart';
+import 'package:switchit/network/network_firestore_controller.dart';
+
+class LoginStatus {
+  static String success = "success";
+  static String error = "error";
+  static String userNotFound = "No user found for that email.";
+  static String wrongPassword = "Wrong password provided for that user.";
+}
+
+class SignUpStatus {
+  static String success = "success";
+  static String emailAddressRegistered =
+      "The email address is already in use by another account.";
+  static String error = "error";
+}
+
+class NetworkAuthController {
+  static bool isUserAuthenticated() {
+    var currentUser = FirebaseAuth.instance.currentUser;
+
+    if (currentUser != null) {
+      debugPrint("FirebaseAuth (isUserAuthenticated): User is authenticated");
+      return true;
+    } else {
+      debugPrint(
+          "FirebaseAuth (isUserAuthenticated): User is not authenticated");
+      return false;
+    }
+  }
+
+  static Future<String> login(String email, String password) async {
+    try {
+      final result = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      debugPrint('FirebaseAuth (signInWithEmailAndPassword): $result');
+      return LoginStatus.success;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        debugPrint(
+            'FirebaseAuth (signInWithEmailAndPassword): No user found for that email.');
+        return LoginStatus.userNotFound;
+      } else if (e.code == 'wrong-password') {
+        debugPrint(
+            'FirebaseAuth (signInWithEmailAndPassword): Wrong password provided for that user.');
+        return LoginStatus.wrongPassword;
+      }
+      return LoginStatus.error;
+    }
+  }
+
+  static Future<String> signUp(
+      String name, String surname, String email, String password) async {
+    try {
+      final result = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      debugPrint('FirebaseAuth (createUserWithEmailAndPassword): $result');
+
+      var isAddedToCloud =
+          await NetworkFirestoreController.addUserToCloud(name, surname, email);
+
+      if (!isAddedToCloud) {
+        return SignUpStatus.error;
+      }
+
+      return SignUpStatus.success;
+    } on FirebaseAuthException catch (e) {
+      debugPrint('FirebaseAuth (createUserWithEmailAndPassword): $e');
+
+      if (e.message == SignUpStatus.emailAddressRegistered) {
+        return SignUpStatus.emailAddressRegistered;
+      }
+      return SignUpStatus.error;
+    }
+  }
+
+  static Future<bool> logout() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      debugPrint('FirebaseAuth (logout): success');
+
+      return true;
+    } on FirebaseAuthException catch (e) {
+      debugPrint('FirebaseAuth (logout): $e');
+
+      return false;
+    }
+  }
+
+  static Future<bool> resetPassword(String email) async {
+    try {
+      await FirebaseAuth.instance.setLanguageCode("en");
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+
+      debugPrint('FirebaseAuth (resetPassword): success');
+
+      return true;
+    } on FirebaseAuthException catch (e) {
+      debugPrint('FirebaseAuth (resetPassword): $e');
+
+      return false;
+    }
+  }
+
+  static Future<bool> deleteAccount() async {
+    try {
+      final user = FirebaseAuth.instance.currentUser;
+      await user?.delete();
+
+      debugPrint('FirebaseAuth (deleteAccount): success');
+
+      var userDocId = await DatabaseRealm().getUserDocId();
+      var isRemovedFromCloud =
+          await NetworkFirestoreController.removeUserFromCloud(userDocId!);
+
+      if (!isRemovedFromCloud) {
+        return false;
+      }
+
+      return true;
+    } on FirebaseAuthException catch (e) {
+      debugPrint('FirebaseAuth (deleteAccount): $e');
+
+      return false;
+    }
+  }
+}
