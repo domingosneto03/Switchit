b92e3087ce727ba02775116b390f1176d65acd2d
Artur Telo <93836583+ArturTelo@users.noreply.github.com>
2023-04-06 14:58:43 +0100

    Update routes.dart

--- app/lib/app/routes.dart
+++ app/lib/app/routes.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:switchit/screens/followers/followers_screen.dart';
-import 'package:switchit/screens/messages/message_screen.dart';
-import 'package:switchit/screens/messages/messages_screen.dart';
-import '../screens/followers/following_screen.dart';
-import '../screens/signup/view/signup_screen.dart';
-import '../screens/credits/credits_screen.dart';
-import '../screens/home/home_screen.dart';
-import '../screens/login/login_screen.dart';
+import 'package:switchit/screens/forgot_password/view/forgot_password_screen.dart';
+import 'package:switchit/screens/home/home_screen.dart';
+import 'package:switchit/screens/login/view/login_screen.dart';
+import 'package:switchit/screens/home/profile/messages/view/details/message_screen.dart';
+import 'package:switchit/screens/signup/view/signup_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   LoginScreen.routeName: (context) => const LoginScreen(),
   SignupScreen.routeName: (context) => const SignupScreen(),
+  ForgotPasswordScreen.routeName: (context) => const ForgotPasswordScreen(),
   HomeScreen.routeName: (context) => const HomeScreen(),
-  CreditsScreen.routeName: (context) => const CreditsScreen(),
-  MessagesScreen.routeName: (context) => const MessagesScreen(),
   MessageScreen.routeName: (context) => const MessageScreen(),
-  FollowersScreen.routeName: (context) => const FollowersScreen(),
-  FollowingScreen.routeName: (context) => const FollowingScreen(),
 };
