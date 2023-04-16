18f876643acd4025ead651056cc0e3e779d2078f
FranciscoS0usa <fgoncalves032@gmail.com>
2023-04-16 01:47:24 +0100

    improved search screen

--- app/lib/screens/home/search/view/components/body.dart
+++ app/lib/screens/home/search/view/components/body.dart
@@ -1,17 +1,174 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class Body extends StatelessWidget {
-  const Body({super.key});
+class Body extends StatefulWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  late CustomSearchDelegate _delegate;
+
+  List<String> items = [
+    "Apple",
+    "Banana",
+    "Mango",
+    "Pear",
+    "Watermelons",
+    "Blueberries",
+    "Pineapples",
+    "Strawberries"
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _delegate = CustomSearchDelegate();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: SizedBox(
-        width: double.infinity,
-        child: Column(
-          children: const [],
+    return Scaffold(
+        appBar: AppBar(
         ),
-      ),
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton.icon(
+              icon: const Icon(Icons.search),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.black87,
+                backgroundColor: Colors.white54,
+                shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(70))),
+                alignment: Alignment.centerLeft,
+                fixedSize: const Size(387,10),
+              ),
+              label: const Text('Search'),
+              onPressed: () {
+                showSearch(
+                    context: context,
+                    // delegate to customize the search bar
+                    delegate: CustomSearchDelegate()
+                );
+              },
+            ),
+            Expanded(
+              child:Expanded(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text('${items[index]}'),
+                    );
+                  },
+                ),
+              ),
+            )
+          ],
+        )
     );
+  }
+}
+
+class CustomSearchDelegate extends SearchDelegate {
+  // Demo list to show querying
+  List<String> items = [
+    "Apple",
+    "Banana",
+    "Mango",
+    "Pear",
+    "Watermelons",
+    "Blueberries",
+    "Pineapples",
+    "Strawberries"
+  ];
+
+
+  // first overwrite to
+  // clear the search text
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: const Icon(Icons.clear),
+      ),
+    ];
+  }
+
+  // second overwrite to pop out of search menu
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: const Icon(Icons.arrow_back),
+    );
+  }
+
+  // third overwrite to show query result
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var item in items) {
+      if (item.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(item);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+
+  // last overwrite to show the
+  // querying process at the runtime
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    List<String> recentItems = [];
+    if(RawKeyboard.instance.keysPressed.contains(LogicalKeyboardKey.enter)){
+      recentItems.add(query);
+    }
+    if (query.isNotEmpty) {
+      for (var item in items) {
+        if (item.toLowerCase().contains(query.toLowerCase())) {
+          matchQuery.add(item);
+        }
+      }
+      return ListView.builder(
+        itemCount: matchQuery.length,
+        itemBuilder: (context, index) {
+          var result = matchQuery[index];
+          return ListTile(
+            title: Text(result),
+          );
+        },
+      );
+    }
+    else{
+      return ListView.builder(
+        itemCount: recentItems.length,
+        itemBuilder: (context, index) {
+          var result = recentItems[index];
+          return ListTile(
+            title: Text(result),
+          );
+        },
+      );
+    }
   }
 }
