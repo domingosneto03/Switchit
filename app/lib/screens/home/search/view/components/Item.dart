135750f4e360157ce5e68dd9a16f60afd9335508
FranciscoS0usa <fgoncalves032@gmail.com>
2023-04-16 18:09:42 +0100

    improved search screen

---
+++ app/lib/screens/home/search/view/components/Item.dart
@@ -0,0 +1,13 @@
+class Item {
+  final String name;
+  final String description;
+  final double price;
+  final String imageUrl;
+
+  Item({
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+  });
+}
--- app/lib/screens/home/search/view/components/body.dart
+++ app/lib/screens/home/search/view/components/body.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:switchit/screens/home/search/view/components/Item.dart';
 
 class Body extends StatefulWidget {
   const Body({Key? key}) : super(key: key);
@@ -11,15 +11,67 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   late CustomSearchDelegate _delegate;
 
-  List<String> items = [
-    "Apple",
-    "Banana",
-    "Mango",
-    "Pear",
-    "Watermelons",
-    "Blueberries",
-    "Pineapples",
-    "Strawberries"
+  List<Item> itemList = [
+    Item(
+      name: "Red Dress",
+      description: "A beautiful red dress for any occasion",
+      price: 49.99,
+      imageUrl: "https://example.com/red-dress.jpg",
+    ),
+    Item(
+      name: "Black T-Shirt",
+      description: "A comfortable black t-shirt for everyday wear",
+      price: 19.99,
+      imageUrl: "https://example.com/black-tshirt.jpg",
+    ),
+    Item(
+      name: "Blue Jeans",
+      description: "Classic blue jeans that never go out of style",
+      price: 59.99,
+      imageUrl: "https://example.com/blue-jeans.jpg",
+    ),
+    Item(
+      name: "White Sneakers",
+      description: "A pair of stylish and comfortable white sneakers",
+      price: 79.99,
+      imageUrl: "https://example.com/white-sneakers.jpg",
+    ),
+    Item(
+      name: "Leather Jacket",
+      description: "A sleek and stylish black leather jacket",
+      price: 149.99,
+      imageUrl: "https://example.com/leather-jacket.jpg",
+    ),
+    Item(
+      name: "Running Shoes",
+      description: "A pair of lightweight and breathable running shoes",
+      price: 99.99,
+      imageUrl: "https://example.com/running-shoes.jpg",
+    ),
+    Item(
+      name: "Pink Blouse",
+      description: "A feminine and elegant pink blouse",
+      price: 39.99,
+      imageUrl: "https://example.com/pink-blouse.jpg",
+    ),
+    Item(
+      name: "Green Skirt",
+      description: "A flowy and fun green skirt for summer",
+      price: 29.99,
+      imageUrl: "https://example.com/green-skirt.jpg",
+    ),
+    Item(
+      name: "Winter Coat",
+      description: "A warm and cozy coat for cold winter days",
+      price: 129.99,
+      imageUrl: "https://example.com/winter-coat.jpg",
+    ),
+    Item(
+      name: "Striped Top",
+      description: "A cute and casual striped top for everyday wear",
+      price: 24.99,
+      imageUrl: "https://example.com/striped-top.jpg",
+    ),
   ];
 
   @override
@@ -52,7 +104,7 @@ class _BodyState extends State<Body> {
                 showSearch(
                     context: context,
                     // delegate to customize the search bar
-                    delegate: CustomSearchDelegate()
+                    delegate: _delegate
                 );
               },
             ),
@@ -60,10 +112,10 @@ class _BodyState extends State<Body> {
               child:Expanded(
                 child: ListView.builder(
                   shrinkWrap: true,
-                  itemCount: items.length,
+                  itemCount: itemList.length,
                   itemBuilder: (context, index) {
                     return ListTile(
-                      title: Text('${items[index]}'),
+                      title: Text(itemList[index].name),
                     );
                   },
                 ),
@@ -76,20 +128,76 @@ class _BodyState extends State<Body> {
 }
 
 class CustomSearchDelegate extends SearchDelegate {
-  // Demo list to show querying
-  List<String> items = [
-    "Apple",
-    "Banana",
-    "Mango",
-    "Pear",
-    "Watermelons",
-    "Blueberries",
-    "Pineapples",
-    "Strawberries"
+
+  bool recentFlag = true;
+
+  List<Item> itemList = [
+    Item(
+      name: "Red Dress",
+      description: "A beautiful red dress for any occasion",
+      price: 49.99,
+      imageUrl: "https://example.com/red-dress.jpg",
+    ),
+    Item(
+      name: "Black T-Shirt",
+      description: "A comfortable black t-shirt for everyday wear",
+      price: 19.99,
+      imageUrl: "https://example.com/black-tshirt.jpg",
+    ),
+    Item(
+      name: "Blue Jeans",
+      description: "Classic blue jeans that never go out of style",
+      price: 59.99,
+      imageUrl: "https://example.com/blue-jeans.jpg",
+    ),
+    Item(
+      name: "White Sneakers",
+      description: "A pair of stylish and comfortable white sneakers",
+      price: 79.99,
+      imageUrl: "https://example.com/white-sneakers.jpg",
+    ),
+    Item(
+      name: "Leather Jacket",
+      description: "A sleek and stylish black leather jacket",
+      price: 149.99,
+      imageUrl: "https://example.com/leather-jacket.jpg",
+    ),
+    Item(
+      name: "Running Shoes",
+      description: "A pair of lightweight and breathable running shoes",
+      price: 99.99,
+      imageUrl: "https://example.com/running-shoes.jpg",
+    ),
+    Item(
+      name: "Pink Blouse",
+      description: "A feminine and elegant pink blouse",
+      price: 39.99,
+      imageUrl: "https://example.com/pink-blouse.jpg",
+    ),
+    Item(
+      name: "Green Skirt",
+      description: "A flowy and fun green skirt for summer",
+      price: 29.99,
+      imageUrl: "https://example.com/green-skirt.jpg",
+    ),
+    Item(
+      name: "Winter Coat",
+      description: "A warm and cozy coat for cold winter days",
+      price: 129.99,
+      imageUrl: "https://example.com/winter-coat.jpg",
+    ),
+    Item(
+      name: "Striped Top",
+      description: "A cute and casual striped top for everyday wear",
+      price: 24.99,
+      imageUrl: "https://example.com/striped-top.jpg",
+    ),
   ];
 
+  List<dynamic> recent = [];
 
-  // first overwrite to
+  List<Item> suggestionsList=[];
+
   // clear the search text
   @override
   List<Widget>? buildActions(BuildContext context) {
@@ -103,7 +211,7 @@ class CustomSearchDelegate extends SearchDelegate {
     ];
   }
 
-  // second overwrite to pop out of search menu
+  // pop out of search menu
   @override
   Widget? buildLeading(BuildContext context) {
     return IconButton(
@@ -114,61 +222,62 @@ class CustomSearchDelegate extends SearchDelegate {
     );
   }
 
-  // third overwrite to show query result
+  // show query result
   @override
   Widget buildResults(BuildContext context) {
-    List<String> matchQuery = [];
-    for (var item in items) {
-      if (item.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(item);
-      }
-    }
+
     return ListView.builder(
-      itemCount: matchQuery.length,
+      itemCount: suggestionsList.length,
       itemBuilder: (context, index) {
-        var result = matchQuery[index];
+        var result = suggestionsList[index];
         return ListTile(
-          title: Text(result),
+          title: Text(result.name),
         );
       },
     );
   }
 
-  // last overwrite to show the
+
   // querying process at the runtime
   @override
   Widget buildSuggestions(BuildContext context) {
-    List<String> matchQuery = [];
-    List<String> recentItems = [];
-    if(RawKeyboard.instance.keysPressed.contains(LogicalKeyboardKey.enter)){
-      recentItems.add(query);
-    }
-    if (query.isNotEmpty) {
-      for (var item in items) {
-        if (item.toLowerCase().contains(query.toLowerCase())) {
-          matchQuery.add(item);
-        }
-      }
-      return ListView.builder(
-        itemCount: matchQuery.length,
-        itemBuilder: (context, index) {
-          var result = matchQuery[index];
-          return ListTile(
-            title: Text(result),
-          );
-        },
-      );
-    }
-    else{
-      return ListView.builder(
-        itemCount: recentItems.length,
-        itemBuilder: (context, index) {
-          var result = recentItems[index];
-          return ListTile(
-            title: Text(result),
-          );
-        },
-      );
-    }
+    final recentList = query.isEmpty
+        ? recent
+        : itemList.where((p) => p.name.toLowerCase().contains(query.toLowerCase())).toList();
+    return ListView.builder(
+      itemCount: recentList.length,
+      itemBuilder: (context, index) {
+        dynamic result = recentList[index];
+        return GestureDetector(
+          onTap: () {
+            if (result is Item) {
+              if (recent.contains(result)) {
+                recent.remove(result);
+              }
+              recent.insert(0, result);
+              recentFlag = false;
+              showResults(context);
+            }
+          },
+          child: ListTile(
+            title: result is Item ? Text(result.name) : Text(result),
+            leading: result is Item ? const Icon(Icons.image) : const Icon(Icons.search),
+          ),
+        );
+      },
+    );
   }
+
+  @override
+  void showResults(BuildContext context) {
+    super.showResults(context);
+    if(query.isNotEmpty && recentFlag ) {
+      if (recent.contains(query)) {
+        recent.remove(query);
+      }
+      recent.insert(0, query);
+    }
+    recentFlag = true;
+  }
+
 }
