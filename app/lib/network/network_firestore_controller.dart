a4826de28b5e72d08d9801bd61f63251b7372c5e
Artur Telo <arturtelo88@hotmail.com>
2023-04-28 16:27:46 +0100

    Fix get items

--- app/lib/network/network_firestore_controller.dart
+++ app/lib/network/network_firestore_controller.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:switchit/database/database_realm.dart';
 import 'package:switchit/screens/home/profile/items_for_trade/view_model/item_data_model.dart';
 
-import '../screens/home/search/view_model/user_data_model.dart';
+import 'package:switchit/screens/home/search/view_model/user_data_model.dart';
 
 class TableCloudUser {
   static String name = "users";
@@ -24,7 +24,7 @@ class TableCloudItem {
 
 class NetworkFirestoreController {
   static final NetworkFirestoreController _singleton =
-  NetworkFirestoreController._internal();
+      NetworkFirestoreController._internal();
 
   factory NetworkFirestoreController() {
     return _singleton;
@@ -36,7 +36,7 @@ class NetworkFirestoreController {
 
   Future<bool> addUserToCloud(String name, String surname, String email) async {
     CollectionReference users =
-    FirebaseFirestore.instance.collection(TableCloudUser.name);
+        FirebaseFirestore.instance.collection(TableCloudUser.name);
 
     var isAddedToCloud = false;
 
@@ -47,8 +47,7 @@ class NetworkFirestoreController {
     }).then((value) {
       DatabaseRealm().setUser(name, surname, email, value.id);
       debugPrint(
-          "FirebaseFirestore (addUserToCloud): User Added with id: ${value
-              .id}");
+          "FirebaseFirestore (addUserToCloud): User Added with id: ${value.id}");
 
       isAddedToCloud = true;
     }).catchError((error) {
@@ -62,7 +61,7 @@ class NetworkFirestoreController {
 
   Future<bool> getUserFromCloud(String email) async {
     CollectionReference users =
-    FirebaseFirestore.instance.collection(TableCloudUser.name);
+        FirebaseFirestore.instance.collection(TableCloudUser.name);
 
     var success = false;
 
@@ -70,7 +69,7 @@ class NetworkFirestoreController {
         .where(TableCloudUser.fieldUserEmail, isEqualTo: email)
         .get()
         .then(
-          (querySnapshot) async {
+      (querySnapshot) async {
         var user = querySnapshot.docs.first;
         await DatabaseRealm().setUser(
             user.get(TableCloudUser.fieldUserName),
@@ -81,8 +80,7 @@ class NetworkFirestoreController {
         debugPrint("FirebaseFirestore (getUserFromCloud): Success");
         success = true;
       },
-      onError: (e) =>
-      {
+      onError: (e) => {
         debugPrint(
             "FirebaseFirestore (getUserFromCloud): Failed to get user: $e"),
         success = false
@@ -93,7 +91,7 @@ class NetworkFirestoreController {
 
   Future<bool> removeUserFromCloud(String userId) async {
     CollectionReference users =
-    FirebaseFirestore.instance.collection(TableCloudUser.name);
+        FirebaseFirestore.instance.collection(TableCloudUser.name);
 
     var isRemovedFromCloud = false;
 
@@ -111,10 +109,10 @@ class NetworkFirestoreController {
 
   // Item
 
-  Future<bool> addItemCurrentUserToCloud(String name, String description,
-      String location, String imageUrl) async {
+  Future<bool> addItemCurrentUserToCloud(
+      String name, String description, String location, String imageUrl) async {
     CollectionReference users =
-    FirebaseFirestore.instance.collection(TableCloudUser.name);
+        FirebaseFirestore.instance.collection(TableCloudUser.name);
 
     var userDocId = await DatabaseRealm().getUserDocId();
 
@@ -138,11 +136,9 @@ class NetworkFirestoreController {
     return isAddedToCloud;
   }
 
-  Future<List<ItemDataModel>> getItemsCurrentUserCloud() async {
+  Future<List<ItemDataModel>> getItemsUserCloud(String userDocId) async {
     CollectionReference users =
-    FirebaseFirestore.instance.collection(TableCloudUser.name);
-
-    var userDocId = await DatabaseRealm().getUserDocId();
+        FirebaseFirestore.instance.collection(TableCloudUser.name);
 
     List<ItemDataModel> items = [];
 
@@ -172,7 +168,7 @@ class NetworkFirestoreController {
     var userDocId = await DatabaseRealm().getUserDocId();
 
     CollectionReference users =
-    FirebaseFirestore.instance.collection(TableCloudUser.name);
+        FirebaseFirestore.instance.collection(TableCloudUser.name);
 
     var isRemovedFromCloud = false;
 
@@ -193,28 +189,31 @@ class NetworkFirestoreController {
     return isRemovedFromCloud;
   }
 
-  Future<List<UserDataModel>> getAllUsersCloud() async {
+  Future<List<UserDataModel>> getAllItemsCloud() async {
     CollectionReference users =
-    FirebaseFirestore.instance.collection(TableCloudUser.name);
+        FirebaseFirestore.instance.collection(TableCloudUser.name);
 
     var userDocId = await DatabaseRealm().getUserDocId();
 
     List<UserDataModel> usersList = [];
 
-    var data = await users.where('id', isNotEqualTo: userDocId).get();
+    var data = await users.get();
 
     for (var doc in data.docs) {
       String id = doc.id;
-      String name = doc.get(TableCloudUser.fieldUserName);
-      String surname = doc.get(TableCloudUser.fieldUserSurname);
-      String email = doc.get(TableCloudUser.fieldUserEmail);
-      List<ItemDataModel> itemsList = doc.get(TableCloudUser.fieldUserItems);
 
-      debugPrint(
-          "FirebaseFirestore (getItemsCurrentUserCloud): ItemDataModel-> name: $name, surname: $surname, email: $email, items: $itemsList");
+      if (userDocId != id) {
+        String name = doc.get(TableCloudUser.fieldUserName);
+        String surname = doc.get(TableCloudUser.fieldUserSurname);
+        String email = doc.get(TableCloudUser.fieldUserEmail);
 
-      usersList.add(
-          UserDataModel(id, name, surname, email, itemsList));
+        var items = await getItemsUserCloud(doc.id);
+
+        debugPrint(
+            "FirebaseFirestore (getItemsCurrentUserCloud): ItemDataModel-> name: $name, surname: $surname, email: $email, items: $items");
+
+        usersList.add(UserDataModel(id, name, surname, email, items));
+      }
     }
 
     debugPrint("FirebaseFirestore (getItemsCurrentUserCloud): Success");
@@ -222,43 +221,13 @@ class NetworkFirestoreController {
     return usersList;
   }
 
-  Future<List<ItemDataModel>> getItemsUserCloud(UserDataModel user) async {
-    CollectionReference users =
-    FirebaseFirestore.instance.collection(TableCloudUser.name);
-
-    var userDocId = user.id;
-
-    List<ItemDataModel> items = [];
-
-    var data = await users.doc(userDocId).collection(TableCloudItem.name).get();
-
-    for (var doc in data.docs) {
-      String id = doc.id;
-      String name = doc.get(TableCloudItem.fieldItemName);
-      String description = doc.get(TableCloudItem.fieldItemDescription);
-      String location = doc.get(TableCloudItem.fieldItemLocation);
-      bool isTraded = doc.get(TableCloudItem.fieldItemIsTraded);
-      String imageUrl = doc.get(TableCloudItem.fieldItemImageUrl);
-
-      debugPrint(
-          "FirebaseFirestore (getItemsCurrentUserCloud): ItemDataModel-> name: $name, description: $description, location: $location, isTraded: $isTraded, imageUrl: $imageUrl");
-
-      items.add(
-          ItemDataModel(id, name, description, location, isTraded, imageUrl));
-    }
-
-    debugPrint("FirebaseFirestore (getItemsCurrentUserCloud): Success");
-
-    return items;
-  }
-
-  Future<List<ItemDataModel>> getAllItemsCloud() async {
+  Future<List<ItemDataModel>> getAllItems() async {
     List<ItemDataModel> itemsList = [];
-    List<UserDataModel> users = getAllUsersCloud() as List<UserDataModel>;
-    for(UserDataModel user in users){
-      itemsList.addAll(getItemsUserCloud(user) as Iterable<ItemDataModel>);
+    List<UserDataModel> usersData = await getAllItemsCloud();
+
+    for (UserDataModel user in usersData) {
+      itemsList.addAll(user.items);
     }
     return itemsList;
   }
-
 }
