1616b008fa3540bda4e2f86cefc3c194851b9094
FranciscoS0usa <fgoncalves032@gmail.com>
2023-05-01 19:33:51 +0100

    changes to search page and added recent to the search bar

--- app/lib/screens/home/search/view/components/body.dart
+++ app/lib/screens/home/search/view/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_progress_hud/flutter_progress_hud.dart';
 import 'package:provider/provider.dart';
+import 'package:switchit/screens/home/profile/items_for_trade/view_model/item_data_model.dart';
 import 'package:switchit/screens/home/search/view_model/items_to_trade_view_model.dart';
 import 'package:switchit/util/status_view.dart';
 import 'package:switchit/util/ui/components/default_dialog.dart';
@@ -17,37 +18,36 @@ class BodyState extends State<Body> {
 
   TextEditingController editingController = TextEditingController();
 
+  late CustomSearchDelegate _delegate;
+
   @override
   Widget build(BuildContext context) {
     viewModel = context.watch<ItemsForTradeViewModel>();
-
+    _delegate = CustomSearchDelegate();
     return Scaffold(
       body: Column(
         children: [
           const SizedBox(
             height: 10,
           ),
-          Padding(
-              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-              child: TextField(
-                controller: editingController,
-                onChanged: (value) {
-                  viewModel.filterSearchResults(value);
-                },
-                decoration: InputDecoration(
-                    labelText: "Search",
-                    hintText: "Search",
-                    prefixIcon: const Icon(Icons.search),
-                    suffixIcon: IconButton(
-                      onPressed: () {
-                        editingController.clear();
-                        viewModel.filterSearchResults("");
-                      },
-                      icon: const Icon(Icons.clear),
-                    ),
-                    border: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(25.0)))),
-              )),
+          ElevatedButton.icon(
+            icon: const Icon(Icons.search),
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.black87,
+              backgroundColor: Colors.white54,
+              shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(70))),
+              alignment: Alignment.centerLeft,
+              fixedSize: const Size(387,10),
+            ),
+            label: const Text('Search'),
+            onPressed: () {
+              showSearch(
+                  context: context,
+                  // delegate to customize the search bar
+                  delegate: _delegate
+              );
+            },
+          ),
           Expanded(
               flex: 1,
               child: RefreshIndicator(
@@ -129,4 +129,107 @@ class BodyState extends State<Body> {
         break;
     }
   }
+}
+
+class CustomSearchDelegate extends SearchDelegate {
+
+  ItemsForTradeViewModel viewModel = ItemsForTradeViewModel();
+
+  bool recentFlag = true;
+
+  List<dynamic> recent = [];
+
+  List<ItemDataModel> suggestionsList=[];
+
+  // clear the search text
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: const Icon(Icons.clear),
+      ),
+    ];
+  }
+
+  // pop out of search menu
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: const Icon(Icons.arrow_back),
+    );
+  }
+
+  // show query result
+  @override
+  Widget buildResults(BuildContext context) {
+
+    return ListView.builder(
+      itemCount: suggestionsList.length,
+      itemBuilder: (context, index) {
+        var result = suggestionsList[index];
+        return ListTile(
+          title: Text(result.name),
+        );
+      },
+    );
+  }
+
+
+  // querying process at the runtime
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final recentList = query.isEmpty
+        ? recent
+        : viewModel.items.where((p) => p.name.toLowerCase().contains(query.toLowerCase())).toList();
+    return ListView.builder(
+      itemCount: recentList.length,
+      itemBuilder: (context, index) {
+        dynamic result = recentList[index];
+        return GestureDetector(
+          onTap: () {
+            if (result is ItemDataModel) {
+              if (recent.contains(result)) {
+                recent.remove(result);
+              }
+              recent.insert(0, result);
+              recentFlag = false;
+              showResults(context);
+            }
+          },
+              child: ListTile(
+                  title: result is ItemDataModel ? Text(result.name) : Text(result),
+                  leading: result is ItemDataModel
+                      ? CircleAvatar(
+                          radius: 21,
+                          backgroundImage: NetworkImage(result.imageUrl),
+                      )
+                      : const CircleAvatar(
+                          backgroundColor: Colors.grey,
+                          radius: 21,
+                          child : CircleAvatar(foregroundColor: Colors.black87,backgroundColor: Colors.white, child: Icon(Icons.search),)
+                      ),
+                ),
+        );
+      },
+    );
+  }
+
+  @override
+  void showResults(BuildContext context) {
+    super.showResults(context);
+    if(query.isNotEmpty && recentFlag ) {
+      if (recent.contains(query)) {
+        recent.remove(query);
+      }
+      recent.insert(1, query);
+    }
+    recentFlag = true;
+  }
+
 }
