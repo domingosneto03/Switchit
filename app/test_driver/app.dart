0eef9528806bb0a95eb00b21be16a1f2a7e9a14a
FranciscoS0usa <fgoncalves032@gmail.com>
2023-05-17 18:46:42 +0100

    started working on gherkin tests

---
+++ app/test_driver/app.dart
@@ -0,0 +1,12 @@
+import 'package:switchit/app/main.dart' as MyApp;
+import 'package:flutter/widgets.dart';
+import 'package:flutter_driver/driver_extension.dart';
+
+void main() {
+  // This line enables the extension
+  enableFlutterDriverExtension();
+
+  // Call the `main()` function of your app or call `runApp` with any widget you
+  // are interested in testing.
+  MyApp.main();
+}
